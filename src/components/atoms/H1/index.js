878542b1f7ca0c2c20d8c3d366a3c05c203fa839
JMcEvoy <jmcevoy@palo-it.com>
2022-03-09 14:07:01 +1100

    initial commit

---
+++ src/components/atoms/H1/index.js
@@ -0,0 +1,5 @@
+const H1 = () => {
+  return <h1>This is a heading</h1>
+}
+
+export default H1
---
+++ src/components/atoms/H1/index.test.js
@@ -0,0 +1,5 @@
+describe('H1 tests', () => {
+  it('should render h1 correctly', () => {
+    //TODO: add test
+  })
+})
